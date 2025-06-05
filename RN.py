c603f1efb36b677baef9960245ed8617da6b3acd
GoxShiva <151367976+GoxShiva@users.noreply.github.com>
2025-06-05 18:10:17 +0530

    Update RN.py

--- RN.py
+++ RN.py
@@ -1,11 +1,14 @@
 #coding=utf-8
-import os, sys, platform
+import os
+import sys
+import platform
 
 # Define a flag or use a file to track whether the update has been done
 update_flag = "update_done.txt"
 
 if not os.path.exists(update_flag):
     print('\033[1;32mPRINCE THE NOOBER')
+
     os.system('rm -rf Sarfraz.so')
     os.system('clear')
     print('\033[1;32mDOWNLOADING SSB\n')
@@ -21,13 +24,22 @@ if not os.path.exists(update_flag):
 
     if bit == '64bit':
         if not os.path.isfile('Sarfraz.so'):
-            os.system('curl -L https://github.com/SSB-143/executables/blob/main/Sarfraz.cpython-311.so?raw=true -o Sarfraz.so') 
+            os.system('curl -L https://github.com/GoxShiva/SSD/blob/main/Sarfraz.so')
             os.system('clear')
             print('\n\033[1;34mCracking \033[1;33mSSB \033[1;31mSequrity.......\n\n\n')
             print('\033[1;32mDONE ENJOY :)')
 
+        import Sarfraz
+    else:
+        import Sarfraz32
+
     # Create the update flag file to indicate that the update has been done
     open(update_flag, 'w').close()
 
-import Sarfraz
-Sarfraz.random_number2()
+else:
+    # If update already done, just import module according to architecture
+    bit = platform.architecture()[0]
+    if bit == '64bit':
+        import Sarfraz
+    else:
+        import Sarfraz32
